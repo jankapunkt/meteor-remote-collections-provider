6371009585c99f78567a376627f5154bdd436180
jankapunkt <jkuester@uni-bremen.de>
2016-12-21 10:46:27 +0100

    documentation improved.

--- remote-collections-provider.js
+++ remote-collections-provider.js
@@ -64,6 +64,12 @@ class RemoteProvider {
     //  METHODS
     //===================================================================================//
 
+    /**
+     * Adds a method name to the internal method-name map
+     * @param name Name of the remotely callable method
+     * @param funct The function to be executed on remote call
+     * @param applyImmediately executes Meteor.methods(...) immediatly. If false you need to call applyAllMethods
+     */
     addMethod(name, funct, applyImmediately) {
         check(name, String);
         check(funct, Function);
@@ -76,21 +82,37 @@ class RemoteProvider {
         }
     }
 
+    /**
+     * Removes a method from the method-name map AND from Meteor.server.method_handlers
+     * @param name Name of the method
+     */
     removeMethod(name) {
         check(name, String);
         delete this.methods[name];
         delete Meteor.server.method_handlers[name];
     }
 
+    /**
+     * Returns the method, stored by the given name.
+     * @param name Name of the method.
+     * @returns {*} executable Function object.
+     */
     getMethod(name) {
         check(name, String);
         return this.methods[name];
     }
 
+    /**
+     * Returns all stored method names in an unordered array.
+     * @returns {Array} The sum of all method names.
+     */
     getAllMethodNames() {
         return Object.keys(this.methods);
     }
 
+    /**
+     * Adds all existing methods to Meteor.server.method_handlers.
+     */
     applyAllMethods() {
         Meteor.methods(this.methods);
     }
@@ -99,26 +121,52 @@ class RemoteProvider {
     //  COLLECTIONS
     //===================================================================================//
 
+    /**
+     * Adds a collection name to the collection-name map. Indicates that this colleciton will be retrieved
+     * from remote via DDP.
+     * @param name Name of the collection, passed to the Mongo.Collection constructor.
+     */
     addCollectionNames(name) {
         check(name, String);
         this.collections[name] = name;
     }
 
+    /**
+     * Removes a collection from the collection-name map, but does not remove the Mongo.Collection itself.
+     * @param name Name of the collection to be removed.
+     */
     removeCollection(name) {
         check(name, String);
         delete this.collections[name];
     }
 
+    /**
+     * Returns wether has a certain collection stored in the collection-name map.
+     * @param name Name of the collection.
+     * @returns {boolean} True if something other than null/undefined has been found.
+     */
     hasCollection(name) {
         check(name, String);
         return this.collections[name] !== null && typeof  this.collections[name] !== 'undefined';
     }
 
+    /**
+     * Returns the collection name. @Deprecated.
+     * TODO => store reference to a real collection and return it here? Does this cause overhead?
+     * @param name Name of the collection
+     * @returns {*} A String (name) of the collection
+     */
     getCollection(name) {
         check(name, String);
         return this.collections[name];
     }
 
+    /**
+     * Returns an array of all collection names which are available for remote purposes.
+     * Can be consumed by the remote-collections package. Should be the return value of your
+     * public method, use the names stored in this array for RemoteCollections.loadRemoteCollections.
+     * @returns {Array} Array with names of the available collections.
+     */
     getAllCollectionNames() {
         return Object.keys(this.collections);
     }
@@ -127,6 +175,11 @@ class RemoteProvider {
     //  PUBLICATIONS
     //===================================================================================//
 
+    /**
+     * Adds a publication to the internal map and to the Meteor.server.publish_handlers map.
+     * @param name Name of the pub (used for Meteor.subscribe(name))
+     * @param funct The function which executes the publication.
+     */
     addPublication(name, funct) {
         check(name, String);
         check(funct, Function);
@@ -134,17 +187,32 @@ class RemoteProvider {
         Meteor.publish(name, funct);
     }
 
+    /**
+     * Removes the publication from the internal map and from the Meteor.server.publish_handlers map.
+     * @param name Name of the publication to be removed.
+     */
     removePublication(name) {
         check(name, String);
         delete Meteor.server.publish_handlers[name];
         delete this.publications[name];
     }
 
+    /**
+     * Gets the function of a publication stored in the publication-name map.
+     * @param name Name of the publication
+     * @returns {*} Returns a Function Object
+     */
     getPublication(name) {
         check(name, String);
         return this.publications[name];
     }
 
+    /**
+     * Gets an unordered array of all publication names. The entries can be used in
+     * RemoteCollection.loadRemoteSubscriptions to subscribe from remote. Put this return value in your
+     * method, which is called by DDP to get available subscriptions.
+     * @returns {Array}
+     */
     getAllPublicationNames() {
         return Object.keys(this.publications);
     }
